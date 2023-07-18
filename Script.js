93366d0233686978d367d77fbd080a2381a1d12c
Collins Emezulu <collinsemezulu63@gmail.com>
2023-07-18 13:58:39 +0100

    Second Commit

---
+++ Script.js
@@ -0,0 +1,15 @@
+let nav = document.querySelector("nav");
+let Bars = document.querySelector(".bars");
+let Line1 = document.querySelector(".line1");
+let Line2 = document.querySelector(".line2");
+let Line3 = document.querySelector(".line3");
+Bars.onclick = function () {
+  nav.classList.toggle("active");
+};
+
+//   Line1.classList.toggle("active-line1");
+//   Line2.classList.toggle("active-line2");
+//   Line3.classList.toggle("active-line3");
+// console.log(Bars)
+// let Bars = document.querySelector('.bars')
+// console.log(Nav)
